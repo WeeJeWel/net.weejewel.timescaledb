23bf58837422dbd4d8816435dacfd5eb8f7514e9
Emile Nijssen <emile@emile.nl>
2026-03-14 10:24:29 +0100

    add support for variables

--- app.mjs
+++ app.mjs
@@ -5,6 +5,9 @@ import { Sequelize } from 'sequelize';
 export default class TimescaleDBApp extends Homey.App {
 
   async onInit() {
+    this.tableNameDevices = 'devices';
+    this.tableNameVariables = 'variables';
+
     // Get Homey ID
     this.homeyId = await this.homey.cloud.getHomeyId();
 
@@ -25,6 +28,15 @@ export default class TimescaleDBApp extends Homey.App {
     // Initialize new Devices
     this.homeyApi.devices.on('device.create', device => {
       this.__initDevice(device);
+    });
+
+    // Initialize all Variables
+    await this.homeyApi.logic.connect();
+    this.homeyApi.logic.on('variable.create', variable => {
+      this.__onVariableChange(variable);
+    });
+    this.homeyApi.logic.on('variable.update', variable => {
+      this.__onVariableChange(variable);
     });
 
     // Connect to TimescaleDB
@@ -93,8 +105,19 @@ export default class TimescaleDBApp extends Homey.App {
     await this.sequelize.authenticate();
     this.log('Connected to TimescaleDB.');
 
-    // Create model for Entry
-    this.table = this.sequelize.define('entry', {
+    // Legacy — Check if a table named `homey` exists and if it has entries.
+    // If so, use the table name 'homey' for devices.
+    const [results] = await this.sequelize.query(`SELECT to_regclass('homey') AS exists;`);
+    if (results[0].exists) {
+      const [countResults] = await this.sequelize.query(`SELECT COUNT(*) AS count FROM homey;`);
+      if (countResults[0].count > 0) {
+        this.tableNameDevices = 'homey';
+        this.log('Legacy table "homey" found with entries. Using "homey" as devices table.');
+      }
+    }
+
+    // Create models
+    this.tableDevices = this.sequelize.define('device', {
       homey_id: {
         type: Sequelize.STRING(24), // Homey ID
         primaryKey: true,
@@ -115,31 +138,72 @@ export default class TimescaleDBApp extends Homey.App {
         type: Sequelize.DECIMAL,
       },
     }, {
-      tableName: 'homey',
+      tableName: this.tableNameDevices,
+      timestamps: false,
+    });
+
+    this.tableVariables = this.sequelize.define('variable', {
+      homey_id: {
+        type: Sequelize.STRING(24), // Homey ID
+        primaryKey: true,
+      },
+      variable_id: {
+        type: Sequelize.STRING(36), // Variable ID
+        primaryKey: true,
+      },
+      time: {
+        type: Sequelize.DATE,
+        primaryKey: true,
+      },
+      value: {
+        type: Sequelize.DECIMAL,
+      },
+    }, {
+      tableName: this.tableNameVariables,
       timestamps: false,
     });
 
     // Ensure table exists
-    await this.table.sync();
-    this.log('Homey table is ready.');
+    await this.tableDevices.sync();
+    await this.tableVariables.sync();
+    this.log('Tables are ready.');
 
-    // Enable Hypertable and Compression
+    // Devices — Enable Hypertable and Compression
     await Promise.resolve().then(async () => {
       // Ensure table is a HyperTable
       await this.sequelize
-        .query("SELECT create_hypertable('homey', 'time', if_not_exists => TRUE);")
+        .query(`SELECT create_hypertable('${this.tableNameDevices}', 'time', if_not_exists => TRUE);`)
         .catch((err) => { throw new Error(`Error Creating Hypertable: ${err.message}`); });
 
       // Enable Compression
       await this.sequelize
-        .query("ALTER TABLE homey SET (timescaledb.compress, timescaledb.compress_segmentby = 'homey_id, device_id, capability_id');")
+        .query(`ALTER TABLE ${this.tableNameDevices} SET (timescaledb.compress, timescaledb.compress_segmentby = 'homey_id, device_id, capability_id');`)
         .catch(err => { throw new Error(`Error Enabling Compression: ${err.message}`); });
 
       await this.sequelize
-        .query("SELECT add_compression_policy('homey', INTERVAL '7 days', if_not_exists => TRUE);")
+        .query(`SELECT add_compression_policy('${this.tableNameDevices}', INTERVAL '7 days', if_not_exists => TRUE);`)
         .catch(err => { throw new Error(`Error Adding Compression Policy: ${err.message}`); });
     })
-      .then(() => this.log('Compression enabled on Homey table.'))
+      .then(() => this.log('Compression enabled on Devices table.'))
+      .catch(err => this.error(`Error setting up Hypertable: ${err.message}`));
+
+    // Variables — Enable Hypertable and Compression
+    await Promise.resolve().then(async () => {
+      // Ensure table is a HyperTable
+      await this.sequelize
+        .query(`SELECT create_hypertable('${this.tableNameVariables}', 'time', if_not_exists => TRUE);`)
+        .catch((err) => { throw new Error(`Error Creating Hypertable: ${err.message}`); });
+
+      // Enable Compression
+      await this.sequelize
+        .query(`ALTER TABLE ${this.tableNameVariables} SET (timescaledb.compress, timescaledb.compress_segmentby = 'homey_id, variable_id');`)
+        .catch(err => { throw new Error(`Error Enabling Compression: ${err.message}`); });
+
+      await this.sequelize
+        .query(`SELECT add_compression_policy('${this.tableNameVariables}', INTERVAL '7 days', if_not_exists => TRUE);`)
+        .catch(err => { throw new Error(`Error Adding Compression Policy: ${err.message}`); });
+    })
+      .then(() => this.log('Compression enabled on Variables table.'))
       .catch(err => this.error(`Error setting up Hypertable: ${err.message}`));
   }
 
@@ -159,20 +223,41 @@ export default class TimescaleDBApp extends Homey.App {
           return;
         }
 
-        if (this.table) {
-          this.table.create({
+        if (this.tableDevices) {
+          this.tableDevices.create({
             homey_id: this.homeyId,
             device_id: deviceId,
             capability_id: capabilityId,
             value,
             time: new Date().getTime(),
-          }).catch(err => this.error(`Error inserting entry: ${err}`));
+          }).catch(err => this.error(`Error inserting Device Entry: ${err}`));
         }
       });
     })
       .then(() => this.log(`[Device:${deviceId}] Initialized`))
       .catch(err => this.error(`[Device:${deviceId}] Error Initializing: ${err}`));
+  }
 
+  __onVariableChange(variable) {
+    const variableId = variable.id;
+    let value = variable.value;
+
+    if (variable.type === 'boolean') {
+      value = value ? 1 : 0;
+    }
+
+    if (typeof value !== 'number') {
+      return;
+    }
+
+    if (this.tableVariables) {
+      this.tableVariables.create({
+        homey_id: this.homeyId,
+        variable_id: variableId,
+        value,
+        time: new Date().getTime(),
+      }).catch(err => this.error(`Error inserting Variable Entry: ${err}`));
+    }
   }
 
 };
